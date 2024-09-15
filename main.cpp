c0531329ba3549c1b5a98dd0925b0e58d44d2b30
Nerocraft4 <blascopau4@gmail.com>
2024-09-15 21:01:34 +0200

    Checkpoint 10-final

--- main.cpp
+++ main.cpp
@@ -12,18 +12,18 @@ int main(){
     hittable_list world;
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5)); 
-    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2)); 
+    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.8); 
     
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100, material_ground));
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.5, material_center));
+    world.add(make_shared<sphere>(point3(0,-0.2,-1), 0.3, material_center));
     world.add(make_shared<sphere>(point3(-1,0,-1), 0.5, material_left));
-    world.add(make_shared<sphere>(point3(1.5,0.5,-0.5), 1, material_right));
+    world.add(make_shared<sphere>(point3(1,0.25,-1), 0.75, material_right));
 
     camera cam;
     cam.aspect_ratio = 16.0/9.0; //make sure these are floats!
     cam.image_width = 400;
-    cam.samples_per_pixel = 100;
+    cam.samples_per_pixel = 200;
     cam.max_depth = 50;
 	//Render
     cam.render(world);
