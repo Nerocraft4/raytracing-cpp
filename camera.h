065a60985cfdb8fb88732b7279df2391dc125cfd
Nerocraft4 <blascopau4@gmail.com>
2024-09-16 00:18:00 +0200

    Checkpoint 13, tutorial error in camera

--- camera.h
+++ camera.h
@@ -10,6 +10,14 @@ class camera {
         int image_width = 100;
         int samples_per_pixel = 10; //nombre de mostres per píxel
         int max_depth = 10; //màxim nombre de rebots
+
+        double vfov = 90;
+        point3 lookfrom = point3(0,0,0);   // Point camera is looking from
+        point3 lookat   = point3(0,0,-1);  // Point camera is looking at
+        vec3   vup      = vec3(0,1,0);     // Camera-relative "up" direction
+
+        double defocus_angle = 0;  // Variation angle of rays through each pixel
+        double focus_dist = 10;    // Distance from camera lookfrom point to plane of perfect focus
         
         void render(const hittable& world) {
             initialize();
@@ -30,10 +38,13 @@ class camera {
     private:
         int image_height;
         double pixel_samples_scale;
-        point3 camera_center;
+        point3 center;
         point3 pixel00_loc;
         vec3 pixel_delta_u;
         vec3 pixel_delta_v;
+        vec3   u, v, w; // Camera frame basis vectors
+        vec3   defocus_disk_u;       // Defocus disk horizontal radius
+        vec3   defocus_disk_v;       // Defocus disk vertical radius
 
         void initialize() {
             image_height = int(image_width / aspect_ratio);
@@ -42,23 +53,35 @@ class camera {
             //AntiAliasing
             pixel_samples_scale = 1.0 / samples_per_pixel;
 
+            center = lookfrom;
+        
             //Camera
-            auto focal_length = 1.0;
-            auto viewport_height = 2.0;
+            auto theta = degrees_to_radians(vfov);
+            auto h = std::tan(theta/2);
+            auto viewport_height = 2*h*focus_dist;
             auto viewport_width = viewport_height * (double(image_width)/image_height);
-            camera_center = point3(0,0,0);
 
+            // Calculate the u,v,w unit basis vectors for the camera coordinate frame.
+            w = unit_vector(lookfrom - lookat);
+            u = unit_vector(cross(vup, w));
+            v = cross(w, u);
+            
             //viewport
-            auto viewport_u = vec3(viewport_width, 0, 0);
-            auto viewport_v = vec3(0, -viewport_height, 0);
+            vec3 viewport_u = viewport_width * u;
+            vec3 viewport_v = viewport_height * v;
 
             //delta u and delta v
             pixel_delta_u = viewport_u / image_width;
             pixel_delta_v = viewport_v / image_height;
 
             //calculate
-            auto viewport_upper_left = camera_center - vec3(0,0, focal_length) - viewport_u/2 - viewport_v/2;
+            auto viewport_upper_left = center - (focus_dist * w) - viewport_u/2 - viewport_v/2;
             pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+            // Calculate the camera defocus disk basis vectors.
+            auto defocus_radius = focus_dist * std::tan(degrees_to_radians(defocus_angle / 2));
+            defocus_disk_u = u * defocus_radius;
+            defocus_disk_v = v * defocus_radius;
         }
         
         ray get_ray(int i, int j) const {
@@ -67,13 +90,20 @@ class camera {
                                 ((i + offset.x()) * pixel_delta_u) + 
                                 ((j + offset.y()) * pixel_delta_v);
 
-            auto ray_origin = camera_center;
+            auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
             auto ray_direction = pixel_sample - ray_origin;
             return ray (ray_origin, ray_direction);
         }
 
         vec3 sample_square() const {
             return vec3(random_double() - 0.5, random_double() - 0.5, 0);
+        }
+
+
+        point3 defocus_disk_sample() const {
+            // Returns a random point in the camera defocus disk.
+            auto p = random_in_unit_disk();
+            return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
         }
         
         color ray_color(const ray& r, int depth, const hittable& world) {
