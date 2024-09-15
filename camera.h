2aa645c881be7cb34f0e689fed78e7f86174ade5
Nerocraft4 <blascopau4@gmail.com>
2024-09-15 17:02:02 +0200

    10.5

--- camera.h
+++ camera.h
@@ -2,6 +2,7 @@
 #define CAMERA_H
 
 #include "hittable.h"
+#include "material.h"
 
 class camera {
     public:
@@ -81,8 +82,11 @@ class camera {
             
             hit_record rec;
             if (world.hit(r, interval(0.001, infinity), rec)) { //0.001 per a evitar shadow acne
-                vec3 direction = rec.normal + random_unit_vector(); //True Lambertian Reflection
-                return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
+                ray scattered;
+                color attenuation;
+                if (rec.mat->scatter(r, rec, attenuation, scattered))
+                    return attenuation * ray_color(scattered, depth-1, world);
+                return color(0,0,0);
             }
 
             vec3 unit_direction = unit_vector(r.direction());
